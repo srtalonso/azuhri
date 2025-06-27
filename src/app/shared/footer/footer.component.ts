430a7afc5b9da8782983c29ca2ef7e7efb6b01da
srtalonso <azu.alonso.93@gmail.com>
2025-06-27 16:18:05 +0200

    parte basica del footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ToolbarModule } from 'primeng/toolbar';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+    //Declaramos variable del año automático
+    currentYear = new Date().getFullYear();
 }
