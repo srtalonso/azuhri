bc41389fe179e266def594f69ab4f2a16d06bd0f
srtalonso <azu.alonso.93@gmail.com>
2025-06-27 13:49:04 +0200

    Primer comit. Crear estructura con carpetas y archivos basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule],
+  imports: [RouterOutlet, ButtonModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RedirectCommand, Routes } from '@angular/router';
+import { ChatComponent } from './features/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Ruta raiz que dirige automaticamnete a chat
+    {path: '', redirectTo: 'chat', pathMatch: 'full'},
+    //Ruta /chat que renderiza ChatComponent
+    {path: 'chat', component: ChatComponent},
+    //Ruta comudin: que se equivoque p ej en la URL, le redirijimos
+    {path: '**', redirectTo: 'chat'}
+];
