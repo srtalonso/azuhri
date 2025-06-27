73895a349d3729768e3014ec73210771895ecc3d
srtalonso <azu.alonso.93@gmail.com>
2025-06-27 16:04:22 +0200

    subir logo Azuhri

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RippleModule } from 'primeng/ripple';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  imports: [ToolbarModule,
+            ButtonModule,
+            RippleModule],
+  templateUrl: './header.component.html',     //Archivo de plantilla html
+  styleUrl: './header.component.css',         //Archivo de stilos css
+  encapsulation: ViewEncapsulation.None       //Desabilita encapsulacon para poder aplicar estilos globales
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  //Vamos  configurar el modo oscuro (true) o claro (false)
+  darkMode = false;
+
+  //Alterna modo claro/oscuro
+  toggleTheme (){
+  this.darkMode = !this.darkMode; // cambia el estado de
+  document.body.classList.toggle('dark', this.darkMode);    //Aplica o remueve clase en el body
+  }
 }
